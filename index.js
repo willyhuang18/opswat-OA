ac77be96314040bc5bbc08b7b9e09fc9439b4a1f
Willy Huang <willyhuang18@gmail.com>
2022-08-24 14:30:52 -0700

    indicate the function to printing the formating result as required

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ const instance = axios.create({
         'apikey': process.env.API_KEY,
     },
 });
+// printing the formating result as required
+const printResults = (json) => {
+    console.log(json)
+    const filename = process.argv[2];
+    const overallStatus = json.scan_results.scan_all_result_a;
+    console.log('---------- SCAN RESULTS ----------');
+    console.log('filename: ', filename);
+    console.log('overall_status: ', overallStatus);
+    console.log('');
+  
+    const scanDetails = json.scan_results.scan_details
+    const engines = Object.keys(scanDetails);
+    for (let engine of engines) {
+        const details = scanDetails[engine];
+        console.log('engine: ', engine);
+        console.log('threat_found: ', details.threat_found);
+        console.log('scan_result: ', details.scan_result_i);
+        console.log('def_time: ', details.def_time);
+        console.log('');
+    }
+    console.log('----------------------------------');
+    console.log('END');
+};
