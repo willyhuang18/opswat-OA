2568f3365519b26a02fcccb5ac301a27c867e2f3
Willy Huang <willyhuang18@gmail.com>
2022-08-24 14:30:17 -0700

    getting the api working

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 require('dotenv').config();
 // using axios to fetching the API endpoint
 const axios = require('axios');
+
+// getting the api working
+const instance = axios.create({
+    baseURL: 'https://api.metadefender.com/v4',
+    headers: {
+        'apikey': process.env.API_KEY,
+    },
+});
